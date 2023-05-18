e9f633e9007e56868bc0fd922e175a052911bbd4
rei315 <alsrnr315@gmail.com>
2023-05-18 10:31:06 +0900

    fix

--- make-xcframework.sh
+++ make-xcframework.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+download_promisekit() {
+    VERSION_FILE="VERSION.txt"
+    OWNER="mxcl"
+    REPO="PromiseKit"
+
+    TAG=$(cat "$VERSION_FILE")
+
+    response=$(curl -s "https://api.github.com/repos/$OWNER/$REPO/releases/tags/$TAG")
+    zip_url=$(echo "$response" | awk -F'"' '/"zipball_url":/ { print $4 }')
+
+    FILE_NAME="PromiseKit.zip"
+
+    curl -LJ "$zip_url" -o "$FILE_NAME"
+    unzip "$FILE_NAME"
+}
+
+find_promisekit_directory() {
+    folder=$(find . -maxdepth 1 -type d -name "mxcl-PromiseKit-*" -print -quit)
+    echo $folder
+}
+
 make_promisekit_xcframework() {
-    git clone https://github.com/mxcl/PromiseKit
-
-    cd PromiseKit
-
+    local directory=$1
+    
+    cd $directory
+    
     xcodebuild archive \
     -scheme PromiseKit \
     -sdk iphonesimulator \
@@ -22,12 +43,17 @@ make_promisekit_xcframework() {
     xcodebuild -create-xcframework \
     -framework build/ios_devices.xcarchive/Products/Library/Frameworks/PromiseKit.framework \
     -framework build/ios_simulators.xcarchive/Products/Library/Frameworks/PromiseKit.framework \
-    -output PromiseKit.xcframework
-
-    mv PromiseKit.xcframework ../XC-PromiseKit/Frameworks
-
-    cd ..
-    rm -rf PromiseKit
+    -output ../PromiseKit.xcframework
 }
 
-make_promisekit_xcframework
+clear_files() {
+    local directory=$1
+    cd ..
+    rm -rf $directory
+    rm -rf PromiseKit.zip
+}
+
+download_promisekit
+promisekit_directory=$(find_promisekit_directory)
+make_promisekit_xcframework $promisekit_directory
+clear_files $promisekit_directory
